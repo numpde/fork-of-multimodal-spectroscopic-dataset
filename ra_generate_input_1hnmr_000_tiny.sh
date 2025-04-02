9f03085feabae5c3ae636266e5b2a1420ae6ea95
RA <>
2025-04-02 22:55:51 +0300

    tiny

--- ra_generate_input_1hnmr_000_tiny.sh
+++ ra_generate_input_1hnmr_000_tiny.sh
@@ -9,13 +9,19 @@ CONDA="${HOME}/Programs/miniconda3/"
 DATA="./data/example_data"
 HNMR_SPLIT_DATASET_DIR="./runs/000_tiny/h_nmr"
 
+USPTO_PATH=$(ls -t $HOME/repos/nmr-msc/code/data/2024-Alberts/06_uspto/a_smiles_uspto_graph_components/*/smile_to_component.tsv.gz | head -n1)
+
 source "${CONDA}/etc/profile.d/conda.sh"
 conda activate fork-of-multimodal-spectroscopic-dataset
 
 python ./benchmark/generate_input.py \
       --analytical_data "${DATA}" \
       --out_path "${HNMR_SPLIT_DATASET_DIR}" \
+      --uspto_path "${USPTO_PATH}" \
       --formula \
+      --explicit_h \
+      --req_stereo \
+      --mono \
       --h_nmr
 
 # Zip the split dataset
